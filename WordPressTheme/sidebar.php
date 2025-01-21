7a213715347b483e870a1d5652f5a70c3e4e0064
ryu.ryu <ryumiya51+53@gmail.com>
2025-01-21 19:08:49 +0900

    ブログカードのホバーアニメーションをトップページにも反映 25.01.21

--- WordPressTheme/sidebar.php
+++ WordPressTheme/sidebar.php
@@ -23,7 +23,7 @@
               <picture class="article-card__img">
                 <?php if ( get_the_post_thumbnail() ) : ?>
                   <source srcset="<?php the_post_thumbnail_url('full'); ?>" type="image/webp">
-                  <img src="<?php the_post_thumbnail_url('full'); ?>" class="article-card__image" alt="<?php the_title(); ?>のアイキャッチ画像">
+                  <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>のアイキャッチ画像">
                 <?php else : ?>
                   <img src="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/noimage.png" alt="noimage">
                 <?php endif; ?>
