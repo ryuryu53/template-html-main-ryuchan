e655613868411f8c351907efe95bf1de8d525e60
ryu.ryu <ryumiya51+53@gmail.com>
2025-07-25 22:09:21 +0900

    Informationページ：タブ修正 25.07.25

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -195,15 +195,18 @@ jQuery(function ($) {
    * -------------------------------------------- */
   function updateBoxShadow() {
     var browserW = window.innerWidth;
-    if (browserW >= 768) {
-      $('.tab__item').each(function () {
+    $('.tab__item').each(function () {
+      if (browserW >= 768) {
         if (!$(this).hasClass('is-active')) {
           $(this).css('box-shadow', 'none');
         } else {
           $(this).css('box-shadow', '0.125rem 0.125rem 0.25rem rgba(0, 0, 0, 0.25)'); // 元のスタイルを指定
         }
-      });
-    }
+      } else {
+        // 768px未満なら全てにデフォルトのbox-shadowを適用
+        $(this).css('box-shadow', '0.125rem 0.125rem 0.25rem rgba(0, 0, 0, 0.25)');
+      }
+    });
   }
 
   /* --------------------------------------------
