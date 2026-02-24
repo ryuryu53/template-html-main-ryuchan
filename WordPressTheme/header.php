e06477630fc67e6b6be8fb96e35ba18f2bed374d
ryu.ryu <ryumiya51+53@gmail.com>
2026-02-24 18:37:16 +0900

    ①Contactセクション：hrefのtel番号末尾「>」削除 ②js-pc-navのaria-hidden：true → falseに修正 26.02.24

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -72,7 +72,7 @@
           <span></span>
           <span></span>
         </button>
-        <nav class="header__pc-nav pc-nav js-pc-nav" aria-label="ヘッダーナビゲーション" aria-hidden="true">
+        <nav class="header__pc-nav pc-nav js-pc-nav" aria-label="ヘッダーナビゲーション" aria-hidden="false">
           <ul class="pc-nav__items">
             <li class="pc-nav__item">
               <a href="<?php echo $campaign; ?>" class="pc-nav__link">
